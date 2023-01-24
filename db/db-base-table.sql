53bd1bc2f4e7a84d1677fc8a1b43b91f2f1fc912
uch2ha <sinyavskiy.d@gmail.com>
2023-01-24 15:30:52 +0200

    create post not working, user_id is missing

--- db/db-base-table.sql
+++ db/db-base-table.sql
@@ -20,14 +20,7 @@ INSERT INTO users (user_name, email, password) VALUES ('Dima', 'dima@dima.com', 
 
 -- POSTS --
 
-INSERT INTO posts (title, context, user_id) VALUES ('First Step', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Possimus eveniet sed officia soluta hic, error vitae, laborum debitis velit totam temporibus! Earum aliquam nostrum eos hic rem, 
-similique voluptas rerum ullam, aperiam obcaecati dicta. Nobis inventore porro expedita assumenda voluptates dicta! 
-Quo officia blanditiis quos natus soluta ea amet, laboriosam dolor reprehenderit unde, 
-doloremque tempore eligendi voluptatem dicta non ullam esse quis facilis distinctio officiis ut autem optio debitis! 
-Nulla ratione exercitationem saepe ullam vel nostrum error vitae ea recusandae, nisi ipsum placeat, 
-laudantium facilis similique aperiam dignissimos sequi quaerat assumenda nihil, consequuntur esse quis. Minus modi culpa eos omnis?
-', 1); 
+INSERT INTO posts (title, context, user_id) VALUES ('First Step', 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', 1); 
 
 INSERT INTO posts (title, context, user_id) VALUES ('Second Step', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
 Possimus eveniet sed officia soluta hic, error vitae, laborum debitis velit totam temporibus! Earum aliquam nostrum eos hic rem, 
